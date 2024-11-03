818d7c30be0b0a13aebba4f9e93028ecfd46eedf
Adria Blancafort <adriablancafort@gmail.com>
2024-11-03 15:33:19 +0100

    remove default path

--- medusa-config.ts
+++ medusa-config.ts
@@ -18,7 +18,6 @@ module.exports = defineConfig({
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
     backendUrl: process.env.MEDUSA_BACKEND_URL,
-    path: "/",
   },
   modules: [
     {
